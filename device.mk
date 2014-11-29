CyanogenMod/android_device_samsung_serrano3gxx

This device is not LTE capable
Change-Id: I14979c468ca981f7d3248318b1abd7a2dd955892

device.mk
@@ -16,6 +16,9 @@
 # Inherit from serrano-common
 $(call inherit-product, device/samsung/serrano-common/serrano-common.mk)
 
+# Device overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/serrano3gxx/overlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml

BadBoy97/android_device_samsung_melius3gxx

device.mk
@@ -17,6 +17,9 @@
 $(call inherit-product, device/samsung/melius-common/melius-common.mk)
 $(call inherit-product, device/samsung/melius-common/nfc.mk)
 
+# Device overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/melius3gxx/overlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml